5d160407d33ce10bfa9b762a4fe4eefa1a983062
zulmycortez <zulmycortez@gmail.com>
2018-09-23 17:24:03 -0400

    Final Version

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import promise from "redux-promise";
+
+import reducers from "./reducers";
+import PostsIndex from "./components/posts-index";
+import PostsNew from "./components/posts-new";
+import PostsShow from "./components/posts-show";
+
+import 'bootstrap/dist/css/bootstrap.css'
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} />
+          <Route path="/posts/:id" component={PostsShow} />
+          <Route path="/" component={PostsIndex} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
